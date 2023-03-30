492d26a8a34785f0b809ccf4a90e998d724b853f
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-03-30 12:56:46 -0300

    Primeiro Hello World

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FirstProgram // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
