77bb2162f91e231cf5cfc12fd2fac423d0357289
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-03-30 13:15:39 -0300

    Saída de dados C#

--- FirstProgram/FirstProgram/Program.cs
+++ FirstProgram/FirstProgram/Program.cs
@@ -5,8 +5,21 @@ namespace FirstProgram
 {
     internal class Program {
         static void Main(string[] args) {
-            double salario = 10.35784;
-            int anos = 27;
+
+            //int idade = 27;
+            //double saldo = 10.35467;
+            //string nome = "Patricia";
+            //tecnica do placeholder
+            //Console.WriteLine("{0} tem {1} anos e recebe {2:F3} por mês!", nome, idade, saldo);
+
+            //tecnica da interpolação
+            //Console.WriteLine($"{nome} tem {idade} anos e recebe {saldo:F3} por mês!!");
+
+            //tecnica de concatenação 
+            //Console.WriteLine(nome + "tem " + idade + " anos e recebe " + saldo.ToString("F3") + " por mês!!");
+
+            double saldo = 10.35784;
+            int idade = 27;
             string z = "Patricia";
             char genero = 'F';
 
@@ -15,18 +28,18 @@ namespace FirstProgram
             Console.WriteLine("Até mais.");
 
             Console.WriteLine();
-            Console.WriteLine(salario);
-            Console.WriteLine(salario.ToString("F2"));
-            Console.WriteLine(salario.ToString("F4"));
-            Console.WriteLine(salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
 
             Console.WriteLine();
-            Console.WriteLine("RESULTADO = " + salario);
-            Console.WriteLine("O valor é de " + salario + " reais");
-            Console.WriteLine("O valor é de " + salario.ToString("F2") + " reais");
+            Console.WriteLine("RESULTADO = " + saldo);
+            Console.WriteLine("O valor é de " + saldo + " reais");
+            Console.WriteLine("O valor é de " + saldo.ToString("F2") + " reais");
 
             Console.WriteLine();
-            Console.WriteLine("A paciente " + z + " tem " + anos + " anos e seu sexo é: " + genero);
+            Console.WriteLine( z + " tem " + idade + " anos e seu sexo é: " + genero);
 
             Console.ReadLine();
         }
