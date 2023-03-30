231fa3fe1fdd792e0d5669b85f2314434a2044d8
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2023-03-30 13:02:12 -0300

    saída de dados CSharp

--- FirstProgram/FirstProgram/Program.cs
+++ FirstProgram/FirstProgram/Program.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Globalization;
 
-namespace FirstProgram // Note: actual namespace depends on the project name.
+namespace FirstProgram 
 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            double salario = 10.35784;
+            int anos = 27;
+            string z = "Patricia";
+            char genero = 'F';
+
+            Console.Write("Olá mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
+            Console.WriteLine(salario);
+            Console.WriteLine(salario.ToString("F2"));
+            Console.WriteLine(salario.ToString("F4"));
+            Console.WriteLine(salario.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO = " + salario);
+            Console.WriteLine("O valor é de " + salario + " reais");
+            Console.WriteLine("O valor é de " + salario.ToString("F2") + " reais");
+
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + anos + " anos e seu sexo é: " + genero);
+
+            Console.ReadLine();
         }
     }
 }
